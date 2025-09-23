1b9fab6eed586c581a7db1886c4e14ef9876d66d
Paul JEAN ZOUAOUI <paul.jean-zouaoui@mail.ru>
2025-09-23 16:09:18 +0300

    V5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '84.201.143.11', '']
+ALLOWED_HOSTS = ['localhost', '84.201.143.11', 'taskiyand.zapto.org']
 
 
 # Application definition
